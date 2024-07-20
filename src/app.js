1bf29d06b67a054302669d77ec3ee883528ffe38
SultanMehmood666 <SultanMehmood@@123>
2024-07-20 14:31:48 +0500

    code

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
+const path = require('path');
 const express = require('express');
-const { model } = require('mongoose');
 const app = express();
 
 
+app.use(express.json());
+app.use(express.static(path.join(process.cwd(), "public")));
+
+
 app.get('/*', (req, resp)=>{
-    resp.status(200).json('Hello world');
+    resp.status(200).send(path.join(process.cwd(), "public"));
 })
 
 module.exports={app}
